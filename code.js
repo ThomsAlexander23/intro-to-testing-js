38bec2d42dbf9e6029d78ccf207ae9cf38224bb7
ThomsAlexander23 <thoms.alexander23@gmail.com>
2021-02-09 12:54:03 -0500

    feat: exercise 4 completed test/pass

--- code.js
+++ code.js
@@ -7,7 +7,11 @@
 function sayHello(name) {
     if (name === "Jane") {
         return "Hello, Jane!";
-    } else {
+    }
+    else if (name === "Alex") {
+        return "Hello, Alex!"
+    }
+    else {
         return ""
     }
 }
--- tests.js
+++ tests.js
@@ -22,7 +22,10 @@ describe('sayHello', function() {
     it('should return a string when called', function() {
         expect(typeof sayHello()).toBe('string');
     });
-    it('it should return "Hello, Jane!" when called with Jane', function() {
+    it('should return "Hello, Jane!" when called with Jane', function() {
         expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
+    it('should return "Hello, Alex!" when called with Alex', function() {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
 });
